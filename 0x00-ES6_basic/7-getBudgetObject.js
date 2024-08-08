1c5027f277ae59dfbb90f789465083ae064afb45
MercyAkpan <akpanmeme@gmail.com>
2024-08-08 16:23:05 +0100

    none

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,3 @@
 export default function getBudgetObject(income, gdp, capita) {
-    // shorthand notation for income: income, gdp: gdp, capita: capita
-    // note: income, gdb and capita must have been declared and initialised
-    // else it would throw a ReferenceError
-       // returns the entire object
     return { income, gdp, capita };
   }
