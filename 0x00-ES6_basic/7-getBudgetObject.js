0f6bf4ce1a9fe7635949497e48f4d3f8aedee00f
MercyAkpan <akpanmeme@gmail.com>
2024-08-08 14:23:54 +0100

    Shorthand notation for object attributes creation

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,12 @@
+export default function getBudgetObject(income, gdp, capita) {
+    // shorthand notation for income: income, gdp: gdp, capita: capita
+    // note: income, gdb and capita must have been declared and initialised
+    // else it would throw a ReferenceError
+    const budget = {
+     income,
+     gdp,
+     capita,
+    };
+    // returns the entire object
+    return budget;
+  }
