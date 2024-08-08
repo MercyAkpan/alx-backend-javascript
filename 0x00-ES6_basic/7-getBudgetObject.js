de00dec43dccacfeb9b5612696ff9a971c09b0ec
MercyAkpan <akpanmeme@gmail.com>
2024-08-08 15:34:59 +0100

    This should be new now

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -2,11 +2,6 @@ export default function getBudgetObject(income, gdp, capita) {
     // shorthand notation for income: income, gdp: gdp, capita: capita
     // note: income, gdb and capita must have been declared and initialised
     // else it would throw a ReferenceError
-    // const budget = {
-    //  income,
-    //  gdp,
-    //  capita,
-    // };
-    // returns the entire object
+       // returns the entire object
     return { income, gdp, capita };
   }
