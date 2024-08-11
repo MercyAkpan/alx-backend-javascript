b3d9a21911c456f4cdde96d225bd325ca0e8117d
MercyAkpan <akpanmeme@gmail.com>
2024-08-11 21:09:46 +0100

    thisnow

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,24 @@
-function getResponseFromAPI(){
-    // console.log("in here ");    
-    const url = `https://jsonplaceholder.typicode.com/todos/1`;
-    return fetch(url)
-}
-export default getResponseFromAPI;
-//     .then( response => {
-//         console.log(response.ok);
-//         console.log("in here ");
-//         response.json()
-//         })
-//         //  return response;
+// function getResponseFromAPI(){
+//     const url = `https://jsonplaceholder.typicode.com/todos/1`;
+//     return fetch(url)
 // }
-// // getResponseFromAPI();
+// export default getResponseFromAPI;
+// //     .then( response => {
+// //         console.log(response.ok);
+// //         console.log("in here ");
+// //         response.json()
+// //         })
+// //         //  return response;
+// // }
+// // // getResponseFromAPI();
+
+async function getResponseFromAPI() {
+    try {
+        console.log("in here ");
+        const url = `https://jsonplaceholder.typicode.com/todos/1`;
+        const response = await fetch(url);
+        return response;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
