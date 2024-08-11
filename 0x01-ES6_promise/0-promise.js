1e5e80ea338d8216b9cae50740f6749de1d784c7
MercyAkpan <akpanmeme@gmail.com>
2024-08-11 20:42:57 +0100

    alx

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,14 @@
 function getResponseFromAPI(){
- const url = `https://jsonplaceholder.typicode.com/todos/1`;
+    // console.log("in here ");    
+    const url = `https://jsonplaceholder.typicode.com/todos/1`;
     return fetch(url)
-    .then( response => {
-         return response.json()
-        })
-        //  return response;
 }
+export default getResponseFromAPI;
+//     .then( response => {
+//         console.log(response.ok);
+//         console.log("in here ");
+//         response.json()
+//         })
+//         //  return response;
+// }
+// // getResponseFromAPI();
