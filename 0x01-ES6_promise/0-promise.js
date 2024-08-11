df019e4e52744c28e5f12ba28958dde120428b54
MercyAkpan <akpanmeme@gmail.com>
2024-08-12 00:28:35 +0100

    This is a simple resolved promise function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,12 @@
 function getResponseFromAPI() {
-    return Promise.resolve({ id: 1, title: "Test Todo" });
+    const result = Promise.resolve({ id: 1, title: "Test Todo" });
+    // result is a Promise object that is resolved, so it will always 
+    // return a successful promise, with the defined values above
+    return result;
 }
-
+export default getResponseFromAPI;
+// The below returns a promise as well, fetch is not allowed in the test
 // function getResponseFromAPI(){
 //     const url = `https://jsonplaceholder.typicode.com/todos/1`;
 //     return fetch(url)
 // }
-export default getResponseFromAPI;
