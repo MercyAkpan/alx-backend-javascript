85c2f2bc16c2aa4a88024c5f07f87949a85662ff
MercyAkpan <akpanmeme@gmail.com>
2024-08-12 00:13:25 +0100

    try this

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,9 @@
-function getResponseFromAPI(){
-    const url = `https://jsonplaceholder.typicode.com/todos/1`;
-    return fetch(url)
+function getResponseFromAPI() {
+    return Promise.resolve({ id: 1, title: "Test Todo" });
 }
+
+// function getResponseFromAPI(){
+//     const url = `https://jsonplaceholder.typicode.com/todos/1`;
+//     return fetch(url)
+// }
 export default getResponseFromAPI;
