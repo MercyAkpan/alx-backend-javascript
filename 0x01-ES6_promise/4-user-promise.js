aba82e807b7f6bc36377e9ba6a1af09bbc942fba
MercyAkpan <akpanmeme@gmail.com>
2024-08-12 07:19:28 +0100

    changed

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,10 @@
 export default function signUpUser(firstName, lastName) {
-    // To use "this" to refer to an instance of a class.
-    // SignUpUser need to be defined as a method of a class.
-    // this.firstName = firstName;
-    // else, use the below code
-
-    return Promise.resolve({
-        firstName: firstName,
-        lastName: lastName,
-      });
+  // To use "this" to refer to an instance of a class.
+  // SignUpUser need to be defined as a method of a class.
+  // this.firstName = firstName;
+  // else, use the below code
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
