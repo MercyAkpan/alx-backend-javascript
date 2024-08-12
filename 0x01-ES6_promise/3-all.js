8351e4926ea9b4583be4c437991313c3978131e0
MercyAkpan <akpanmeme@gmail.com>
2024-08-12 13:20:14 +0100

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,22 @@
 // import non-default functions from utils.js
 import { uploadPhoto, createUser } from './utils';
-export default function handleProfileSignup(){
-    try {
-        // Do the below to try the catch block
-        // throw new Error('The fake API is not working currently');
-      return Promise.all([uploadPhoto(), createUser()])
-        .then((values) => {
-          console.log(values[0].body, values[1].firstName, values[1].lastName);
-          const result = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
-          console.log (result);
-        })
-    } catch (error) {
-      console.log('Signup system offline');
-    }
+
+export default function handleProfileSignup() {
+  try {
+    // Do the below to try the catch block
+    // throw new Error('The fake API is not working currently');
+    // Funtion on success to return a Promise.all() promise.
+    return Promise.all([uploadPhoto(), createUser()])
+      .then((values) => {
+        const result = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
+        console.log(result);
+        // return result;
+      })
+      // The .catch doesn't take any error object, simply logs out a message
+      .catch(() => { console.log('Signup system offline'); });
+  } catch (error) {
+    console.log('Signup system offline');
+    return null;
+  }
 }
 // Now you can use uploadPhoto and createUser in this file
