38cf1cc78ebcacfbab014702cf40ef3aed3aad57
MercyAkpan <akpanmeme@gmail.com>
2024-08-12 07:16:33 +0100

    Changed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
-// import functions from utils.js
+// import non-default functions from utils.js
 import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup(){
     try {
+        // Do the below to try the catch block
         // throw new Error('The fake API is not working currently');
       return Promise.all([uploadPhoto(), createUser()])
         .then((values) => {
           console.log(values[0].body, values[1].firstName, values[1].lastName);
+          return values;
         })
     } catch (error) {
       console.log('Signup system offline');
