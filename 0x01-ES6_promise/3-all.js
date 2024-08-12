9f66c4fed327681a9c0a2aa6f9eba7d87db6927c
MercyAkpan <akpanmeme@gmail.com>
2024-08-12 13:19:55 +0100

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,8 @@ export default function handleProfileSignup(){
       return Promise.all([uploadPhoto(), createUser()])
         .then((values) => {
           console.log(values[0].body, values[1].firstName, values[1].lastName);
-          return values;
+          const result = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
+          console.log (result);
         })
     } catch (error) {
       console.log('Signup system offline');
