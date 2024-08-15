950ad11359e4b6e92c2de0761f9e5fdd8ddd8a91
MercyAkpan <akpanmeme@gmail.com>
2024-08-15 08:34:19 +0100

    alx trial1

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,20 @@
+// export default function getBudgetObject(income, gdp, capita) {
+//   // shorthand notation for income: income, gdp: gdp, capita: capita
+//   // note: income, gdb and capita must have been declared and initialised
+//   // else it would throw a ReferenceError
+//   const budget = { income, gdp, capita };
+//   // returns the entire object
+//   return budget;
+// }
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
 export default function getBudgetObject(income, gdp, capita) {
-  // shorthand notation for income: income, gdp: gdp, capita: capita
-  // note: income, gdb and capita must have been declared and initialised
-  // else it would throw a ReferenceError
-  const budget = { income, gdp, capita };
-  // returns the entire object
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
   return budget;
 }
