ab21ad56ac1763603b77d42b4751f09670d408c2
MercyAkpan <akpanmeme@gmail.com>
2024-08-15 08:14:46 +0100

    alx trial1

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,14 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    // idx is the value of the array element
-    // array.indexOf(idx) is the index of the array element
-    // The below code is to modify the value of the array element in place.
-    array[array.indexOf(idx)] = appendString + idx;
-  }
+// export default function appendToEachArrayValue(array, appendString) {
+//   for (const idx of array) {
+//     // idx is the value of the array element
+//     // array.indexOf(idx) is the index of the array element
+//     // The below code is to modify the value of the array element in place.
+//     array[array.indexOf(idx)] = appendString + idx;
+//   }
 
-  return array;
+//   return array;
+// }
+// ABOVE MY CODE ---  BELOW NOT MY CODE
+export default function appendToEachArrayValue(array, appendString) {
+  return array.map((value) => appendString + value);
 }
