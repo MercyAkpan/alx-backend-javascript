64433e235af1e68659bbe6d8f8f6823e14b02656
MercyAkpan <akpanmeme@gmail.com>
2024-09-21 07:08:36 +0100

    alxx

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,19 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 let inputBuffer = '';
 
 rl.on('line', (input) => {
-    // Store the input in a buffer
-    inputBuffer = input;
-    process.stdout.write(`Your name is: ${inputBuffer}\r`);
-    rl.close();
-  });
+  // Store the input in a buffer
+  inputBuffer = input;
+  process.stdout.write(`Your name is: ${inputBuffer}\r`);
+  rl.close();
+});
 rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
-  });
+  console.log('This important software is now closing');
+  process.exit(0);
+});
