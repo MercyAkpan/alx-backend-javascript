de1c04af82a9a50c7bb283e125a2f21098b079d6
MercyAkpan <akpanmeme@gmail.com>
2024-09-22 21:10:24 +0100

    alx

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,14 +4,31 @@
 /* eslint-disable camelcase */
 const { createServer } = require("http");
 
-// Create the server
-const app = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
-});
+// // Create the server
+// const app = createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello Holberton School!');
+// });
 
-// Listen on port 1245
-app.listen(1245);
+// // Listen on port 1245
+// app.listen(1245);
 
-// Export the app
-module.exports = app;
+// // Export the app
+// module.exports = app;
+
+const http = require("http");
+
+// Define a function to create and start the server
+const startServer = () => {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  });
+  server.listen(1245);
+};
+
+// Start the server immediately
+startServer();
+
+// Export the function to allow other modules to use it
+module.exports = startServer;
